7671888dc0ffdad4897a32ca432d145e115fdf77
Yoshinori Sasaki <norinux.s@gmail.com>
2011-04-26 14:59:46 +0900

    add base program for optional logic

--- systop.c
+++ systop.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char * argv[]) {
 
-int i;
-i = 1;
-
-puts (argv[1]);
+    int optc;
+    while(( optc = getopt(argc, argv, "xyzn:")) != -1){
+        switch (optc){
+            case 'x':
+		puts ("x\n");
+                break;
+            case 'y':
+		puts ("y\n");
+                break;
+            case 'z':
+		puts ("z\n");
+                break;
+            case 'n':
+		puts ("n\n");
+                break;
+            default:
+		puts ("default");
+        }
+    }
 
 }
